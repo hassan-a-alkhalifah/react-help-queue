a8f11746c877b56c8c0a00ab48dbca6ccef60bb3
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-12 18:27:57 -0800

    add initial component content, add p tag including first question with button

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationQuestion() {
+  return(
+    <div>
+      <p>Have you gone through all the steps on the Learn How to Program debugging lession?</p>
+      <button>Yes</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestion;
